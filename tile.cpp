27b3bb0d5d80c25805499b5c9feac746aba1aec6
Surinder Kaur <s3785282@student.rmit.edu.au>
2020-09-11 08:42:54 +1000

    implemented tile class

--- tile.cpp
+++ tile.cpp
@@ -1,8 +1,50 @@
-#include <iostream>
 #include "tile.h"
 
-int main(void) {
-    // insert stuff here :)
+Tile::Tile(Colour colour) {
+    this->colour = colour;
+    this->charColour = convertColourToChar(colour);
+    
+}
 
-    return EXIT_SUCCESS;
+Tile::~Tile() {
+
+}
+
+Colour Tile::getColour() {
+    return colour;
+
+}
+
+char Tile::getCharColour() {
+    return charColour;
+}
+
+char Tile::convertColourToChar(Colour colour) {
+    char red = 'R';
+    char yellow = 'Y';
+    char dark_blue = 'B';
+    char light_blue = 'L';
+    char black = 'U';
+    char first_player = 'F';
+    char no_tile = '.';
+
+    char tile = no_tile;
+
+    if(colour == RED) {
+        tile = red;
+    } else if(colour == YELLOW) {
+        tile = yellow;
+    } else if(colour == DARK_BLUE) {
+        tile = dark_blue;
+    } else if(colour == LIGHT_BLUE) {
+        tile = light_blue;
+    } else if(colour == BLACK) {
+        tile = black;
+    } else if(colour == FIRST_PLAYER) {
+        tile = first_player;
+    } else {
+        tile = no_tile;
+    }
+
+    return tile;
 }
--- tile.h
+++ tile.h
@@ -1,6 +1,30 @@
-class Tile {
-    public: 
-    // stuff here
-    private: 
-    // stuff here
+#ifndef TILE_H
+#define TILE_H
+
+enum Colour {
+    RED,
+    YELLOW,
+    DARK_BLUE,
+    LIGHT_BLUE,
+    BLACK,
+    FIRST_PLAYER,
+    NO_TILE,
 };
+
+class Tile {
+public: 
+    Tile(Colour colour);
+    Tile(Tile& other);
+    ~Tile();
+
+    Colour getColour();
+    char getCharColour();
+
+private: 
+    char convertColourToChar(Colour colour);
+
+    Colour colour;
+    char charColour;
+};
+
+#endif // TILE_H
