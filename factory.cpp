242e68256a7387a2949242389944b6a0d6a1486b
Surinder Kaur <s3785282@student.rmit.edu.au>
2020-09-11 22:42:01 +1000

    implemented factory class

--- factory.cpp
+++ factory.cpp
@@ -1,7 +1,69 @@
-#include <iostream>
+#include "factory.h"
 
-int main(void) {
-    // insert stuff here :)
-
-    return EXIT_SUCCESS;
+Factory::Factory(int factoryNumber) {
+    this->factoryNumber = factoryNumber;
 }
+
+Factory::~Factory() {
+    clear();
+}
+
+void Factory::addTile(Tile* tile) {
+    factoryTiles.push_back(tile);
+    
+}
+
+bool Factory::removeTileAt(int index) {
+    bool tileRemoved = false;
+    signed int length = (signed int) factoryTiles.size();
+
+    if(factoryTiles.size() > 0 && index >=0 && index < length) {
+        delete factoryTiles[index];
+        factoryTiles[index] = nullptr;
+
+        for(int i = index; i < length; ++i) {
+            if(i <= length - 2) {
+                factoryTiles[i] = factoryTiles[i+1];
+            }
+        }
+
+        factoryTiles.pop_back();
+        tileRemoved = true;
+    }
+
+    return tileRemoved;
+}
+
+int Factory::size() {
+    return factoryTiles.size();
+}
+
+int Factory::getIndexOfSameColourTile(Colour colour) {
+    int index = INVALID_INDEX;
+    for(int i = 0; i < (signed int) factoryTiles.size(); ++i) {
+        if(factoryTiles[i]->getColour() == colour) {
+            index = i;
+        }
+    }
+
+    return index;
+ }
+
+
+void Factory::clear() {
+    for(int i = 0; i < (signed int) factoryTiles.size(); ++i) {
+        delete factoryTiles[i];
+        factoryTiles[i] = nullptr;
+    }
+    factoryTiles.clear();
+}
+
+int Factory::getFactoryNumber() {
+    return factoryNumber;
+}
+
+//TESTING METHODS
+// Tile* Factory::getTileAt(int index) {
+//     return factoryTiles[index];
+// }
+
---
+++ factory.h
@@ -0,0 +1,44 @@
+#ifndef FACTORY_H
+#define FACTORY_H
+
+#include "tile.h"
+#include <vector>
+
+#define INITIAL_FACTORY_SIZE 4
+#define INVALID_INDEX -1
+
+class Factory {
+public:
+    Factory(int factoryNumber);
+    ~Factory();
+
+    //Adds Tile to the back of the array
+    void addTile(Tile* tile);
+
+    //Removes tile from given index, returns true if tile was removed successfully
+    bool removeTileAt(int index);
+
+    //Returns the size of the factory vector
+    int size();
+
+    //Returns index of rightmost tile of given colour
+    //returns INVALID_INDEX if no tiles of given colour are present 
+    int getIndexOfSameColourTile(Colour colour);
+
+    //Clears the factory vector
+    void clear();
+
+    //returns factory number
+    int getFactoryNumber();
+
+    //TESTING METHOD
+    //Get a Tile at a specifc index 
+    Tile* getTileAt(int index);
+
+private: 
+    int factoryNumber;
+    std::vector<Tile*> factoryTiles;
+
+};
+
+#endif //FACTORY_H
