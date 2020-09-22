9d54d73b16624500d9f4176e73ae294830874712
s3781183 <rushebala@gmail.com>
2020-09-22 11:25:21 +1000

    implemented printPlayerMosaic method

--- gameEngine.cpp
+++ gameEngine.cpp
@@ -25,16 +25,42 @@ void GameEngine::addTileFromFactoryToMosaic() {
 
 }
 
-void GameEngine::printPlayerMosaic(player* player) {
+void GameEngine::printPlayerMosaic(Player* player) {
      Tile** grid = player->getMosaic()->getGrid();
      Tile** patternLine = player->getMosaic()->getPatternLine();
-        for(int i = 0; i < ROWS; ++i) {
-             for(int j = 0; j < COLS; ++j) {
-            std::cout << grid[i]->getCharColour() << "|" << patternLine[i];
-        }
-        std::cout << std::endl;
-    }
+     std::vector<Tile*> brokenTile = player->getMosaic()->getBrokenTiles();
 
+     std::cout << "Mosaic for " << player->getPlayerName()<< ":" << std::endl;
+     int num = 0;
+     //prints first number for first row
+     std::cout << ++num <<": ";
+    //prints patternLine
+     for(int i = 0; i < ROWS*COLS; ++i) {
+        if(patternLine[i] != nullptr) 
+            std::cout << patternLine[i]->getCharColour() << " ";
+
+        else 
+            std::cout << "*";
+
+        if(i%5 == 4) {
+            std::cout <<"||";
+            //prints grid
+             for(int a = 0; a < ROWS; ++a) {
+                std::cout << grid[i]->getCharColour() << " ";   
+            }
+            std::cout << std::endl;
+            //prints number for each row
+            if(num<5){
+                std::cout << ++num <<": ";}
+        }
+    }
+    //prints broken tile
+    std::cout << "broken: " ;
+    for(int i = 0; i < brokenTile.size(); ++i){
+        std::cout << brokenTile.at(i);
+    }
+     std::cout << std::endl;
+    
 }
 
 // int GameEngine::calculatePlayerScores(Player* player) {
