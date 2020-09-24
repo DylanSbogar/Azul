b11b63fcf1a2250f8123bfa41f45a792bce2a2ad
s3781183 <rushebala@gmail.com>
2020-09-24 16:35:36 +1000

    merge conflict

--- gameEngine.cpp
+++ gameEngine.cpp
@@ -244,45 +244,6 @@ void GameEngine::saveGame(string fileName) {
     saveFile << allTurns;
 }
 
-<<<<<<< HEAD
-void GameEngine::printPlayerMosaic(Player* player) {
-     Tile** grid = player->getMosaic()->getGrid();
-     Tile** patternLine = player->getMosaic()->getPatternLine();
-     std::vector<Tile*> brokenTile = player->getMosaic()->getBrokenTiles();
-
-     std::cout << "Mosaic for " << player->getPlayerName()<< ":" << std::endl;
-     int num = 0;
-     //prints first number for first row
-     std::cout << ++num <<": ";
-    //prints patternLine
-     for(int i = 0; i < ROWS*COLS; ++i) {
-        if(patternLine[i] != nullptr) 
-            std::cout << patternLine[i]->getCharColour() << " ";
-
-        else 
-            std::cout << "*";
-
-        if(i%5 == 4) {
-            std::cout <<"||";
-            //prints grid
-             for(int a = 0; a < ROWS; ++a) {
-                std::cout << grid[i]->getCharColour() << " ";   
-            }
-            std::cout << std::endl;
-            //prints number for each row
-            if(num<ROWS){
-                std::cout << ++num <<": ";}
-        }
-    }
-    //prints broken tile
-    std::cout << "broken: " ;
-    for(int i = 0; i < brokenTile.size(); ++i){
-        std::cout << brokenTile.at(i);
-    }
-     std::cout << std::endl;
-    
-}
-=======
 
 void GameEngine::printPlayerMosaic(Player* player) {
     Tile** grid = player->getMosaic()->getGrid();
@@ -293,7 +254,6 @@ void GameEngine::printPlayerMosaic(Player* player) {
     
     //prints first number for first row
     std::cout << ++num <<": ";
->>>>>>> 62b7df87fd2a504362b3cce6e606621e799138ef
 
     //prints patternLine
     for(int i = 0; i < ROWS*COLS; ++i) {
