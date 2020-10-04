0202908abe377b742b88b7276dc636d3d7e5fcbf
s3781183 <rushebala@gmail.com>
2020-10-04 14:46:58 +1100

    fixed playingSingleRound test

--- gameEngine.cpp
+++ gameEngine.cpp
@@ -182,9 +182,10 @@ void GameEngine::runGame() {
 
         //Fill the factories back up
         factories->FillFactoriesFromTileBag(tileBag);
-        
+         if(isLoading == false) {
         //Increment round
         cout << "=== END OF ROUND ===" << endl;
+         }
         ++rounds;
     }
 
